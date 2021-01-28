283cf99970cd78f23013e250356ed724d26e4414
dek6613 <dek6613@rit.edu>
2021-01-28 18:26:07 -0500

    Created system for switches and triggers that affect other objects

---
+++ GGJ2021/Assets/Scripts/ThisTriggersThat.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ThisTriggersThat : MonoBehaviour
+{
+    [SerializeField]
+    Collider trigger;
+    [SerializeField]
+    List<Activated> activated;
+
+    [SerializeField]
+    bool disableWhenTriggered = true;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        foreach(Activated a in activated)
+        {
+            a.OnActivate();
+        }
+
+        if (disableWhenTriggered)
+        {
+            enabled = false;
+        }
+    }
+}
