5db127dcbeb8a3d8a1c5026d3149c3a0a5003adf
theriverpanther <54086176+theriverpanther@users.noreply.github.com>
2021-01-28 01:29:02 -0500

    First Person Camera
    Added a second controller for a first person camera. Works fully functionally with rotation assist, but we need to fix transitions between the two cameras

--- GGJ2021/Assets/Scripts/PlayerControl.cs
+++ GGJ2021/Assets/Scripts/PlayerControl.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Cinemachine;
 
 public class PlayerControl : MonoBehaviour
 {
@@ -10,6 +11,11 @@ public class PlayerControl : MonoBehaviour
     KeyCode jumpKey = KeyCode.Space;
 
     [SerializeField]
+    KeyCode changeCam = KeyCode.LeftShift;
+
+    bool isThirdPerson = true;
+
+    [SerializeField]
     float jumpForceMag = 50f;
     [SerializeField]
     float jumpForceVerticalMod = 1.5f;
@@ -17,16 +23,28 @@ public class PlayerControl : MonoBehaviour
     Rigidbody rigidbody;
     Collider collider;
 
+    [SerializeField]
+    Transform firstPersonCam;
+    [SerializeField]
+    Transform thirdPersonCam;
+
+    Quaternion initRot;
+
     // Start is called before the first frame update
     void Start()
     {
         rigidbody = gameObject.GetComponent<Rigidbody>();
         collider = gameObject.GetComponent<BoxCollider>();
+        thirdPersonCam = gameObject.transform.GetChild(0);
+        firstPersonCam = gameObject.transform.GetChild(1);
+        firstPersonCam.GetComponent<CinemachineCollider>().enabled = false;
     }
 
     // Update is called once per frame
     void Update()
     {
+        // Set the initial rotation for the first person camera before the key bounces
+        initRot = firstPersonCam.transform.rotation;
         if (Input.GetKeyDown(jumpKey) && IsGrounded())
         {
             // Get the direction the main camera is facing
@@ -48,6 +66,42 @@ public class PlayerControl : MonoBehaviour
             torque.z = Random.Range(-jumpForceMag, jumpForceMag);
 
             rigidbody.AddTorque(torque);
+        }
+        // Change the perspective camera in use
+        if(Input.GetKeyDown(changeCam))
+        {
+            isThirdPerson = !isThirdPerson;
+
+            // Find the vector between both cameras
+            Vector3 between = firstPersonCam.position - thirdPersonCam.position;
+
+            // If it is transitioning to third person, make the third person cam look to the direction
+            // Otherwise, make the first person camera look in the opposite direction
+            if(isThirdPerson)
+            {
+                thirdPersonCam.rotation = Quaternion.LookRotation(between);
+            }
+            else
+            {
+                firstPersonCam.rotation = Quaternion.LookRotation(-between);
+            }
+
+            // Swap priority between cameras and enable the correct camera colliders
+            thirdPersonCam.GetComponent<CinemachineFreeLook>().Priority = (isThirdPerson?10:8);
+            thirdPersonCam.GetComponent<CinemachineCollider>().enabled = isThirdPerson;
+            firstPersonCam.GetComponent<CinemachineFreeLook>().Priority = (!isThirdPerson?10:8);
+            firstPersonCam.GetComponent<CinemachineCollider>().enabled = !isThirdPerson;   
+        }
+
+        // TODO: Force the cameras to transition better / at all
+    }
+
+    void LateUpdate()
+    {
+        // Force the first person camera to rotate to it's initial position before the Update method
+        if(!isThirdPerson)
+        {
+            firstPersonCam.transform.rotation = Quaternion.LookRotation(Vector3.forward, Vector3.up);
         }
     }
 
