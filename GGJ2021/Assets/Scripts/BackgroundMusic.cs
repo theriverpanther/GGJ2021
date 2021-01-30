2ebb3f3208b2962e4d400200fbb1a114347c73ce
theriverpanther <54086176+theriverpanther@users.noreply.github.com>
2021-01-29 22:45:59 -0500

    Background Music
    Added in a background music script to loop between any number of background songs provided, and organized the audio folders a bit into SoundFX and Music separately

---
+++ GGJ2021/Assets/Scripts/BackgroundMusic.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BackgroundMusic : MonoBehaviour
+{
+    AudioSource source;
+    [SerializeField]
+    List<AudioClip> songs;
+    int currentSong = 0;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        source = gameObject.GetComponent<AudioSource>();
+        source.loop = true;
+        StartCoroutine(backgroundMusic());
+    }
+
+    IEnumerator backgroundMusic()
+    {
+        source.clip = songs[currentSong];
+        source.Play();
+        yield return new WaitForSeconds(songs[currentSong].length);
+        currentSong++;
+        if(currentSong >= songs.Count)
+        {
+            currentSong = 0;
+        }
+        StartCoroutine(backgroundMusic());
+    }
+}
